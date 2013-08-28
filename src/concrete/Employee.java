bdc3f0309504cb73403610b96025adf506d524f8
Tracy Kobel <Tracy Kobel@192.168.1.106>
2013-08-28 15:23:31 -0500

    finished concrete package

--- src/concrete/Employee.java
+++ src/concrete/Employee.java
@@ -27,4 +27,10 @@ public class Employee {
     public int getAge() {
         return age;
     }
+    public void displayInfo(){
+        System.out.println("Employee No.: " + empId);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Position: " + position);
+    }
 }
